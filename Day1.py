20c1cc0f38a7b5416dc14761a7a38454bcdef2f1
Sergio B <146890934+Xilithh@users.noreply.github.com>
2024-04-22 16:31:05 -0500

    Update Day1.py
    Day 1 part 1 finished

--- Day1.py
+++ Day1.py
@@ -1,4 +1,4 @@
-import numbers
+#part 1
 
 def find_numbers(filename):
     with open(filename, 'r')as file:
@@ -10,14 +10,41 @@ def find_numbers(filename):
             i = i.strip()
             if i:
                 numbers = [char for char in i if char.isdigit()]
-                print(numbers)
+                
+
+            if len(numbers) <= 1:
+                 final_numbers.append(int(numbers[0]) * 11)
+                 
+            if len(numbers) >= 2:
+                 final_numbers.append(int(numbers[0] + numbers[-1]))
+                
+    code = sum(final_numbers)
+
+    print(code)
+    
 
 
-
-
-file = 'Insert File Name Here'
+file = 'placeholder.txt'
 
 find_numbers(file)
+
+
+
+#part 2
+
+word_to_digit = {
+'zero':0,
+"one":1,
+'two':2,
+'three':3,
+'four':4,
+'five':5,
+'six':6,
+'seven':7,
+'eight':8,
+'nine':9
+}
+
 
 
 
